b85700cf3595d5d3ee2da6480ce9912789da95d2
Louis Garman <louisgarman@gmail.com>
2020-02-23 12:23:28 +0000

    Operation CRD

---
+++ pkg/apis/goalspike/v1alpha1/operation_types.go
@@ -0,0 +1,52 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// OperationSpec defines the desired state of Operation
+type OperationSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Tarball   string   `json:"tarball"`
+	Command   string   `json:"command"`
+	Args      []string `json:"args"`
+	Workspace string   `json:"workspace"`
+}
+
+// OperationStatus defines the observed state of Operation
+type OperationStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Operation is the Schema for the operations API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=operations,scope=Namespaced
+type Operation struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OperationSpec   `json:"spec,omitempty"`
+	Status OperationStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// OperationList contains a list of Operation
+type OperationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Operation `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Operation{}, &OperationList{})
+}
