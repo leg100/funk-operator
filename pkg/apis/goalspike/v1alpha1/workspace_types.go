0a35184dce16ac86206b01693faf15e27e20cd51
Louis Garman <louisgarman@gmail.com>
2020-02-23 12:13:21 +0000

    Workspace has no fields

--- pkg/apis/goalspike/v1alpha1/workspace_types.go
+++ pkg/apis/goalspike/v1alpha1/workspace_types.go
@@ -12,7 +12,6 @@ type WorkspaceSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-	Bucket string `json:"bucket"`
 }
 
 // WorkspaceStatus defines the observed state of Workspace
